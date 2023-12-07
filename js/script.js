f4adc7106918f27b275ba4a64f07d3a77b654ab5
Santiago Lanz <Santiagolanz019@gmail.com>
2023-12-07 16:28:18 -0300

    Products Section v.4

--- js/script.js
+++ js/script.js
@@ -9,40 +9,56 @@ function getData(JSON, proper) {
     })
 }
 function fillCarousel(prop, card, id) {
+  propMap = {
+    juguetes_gatos: ['item-carousel-1', 'item-carousel-2', 'item-carousel-3'],
+    healthcare_gatos: ['item-carousel-4', 'item-carousel-5', 'item-carousel-6']
+  };
+  let index = Math.min(Math.floor((id - 1) / 3), propMap[prop].length - 1);
+  let elId = propMap[prop][index];
+  document.getElementById(elId).innerHTML += card;
+}
+
+function fillCarouselSm(prop, item) {
+  let toys = document.getElementById("smToys");
+  let healthcare = document.getElementById("smHealthcare");
   if (prop === "juguetes_gatos") {
-    if (id <= 3) {
-      document.getElementById('item-carousel-1').innerHTML += card;
-    } else if (id > 3 && id <= 6) {
-      document.getElementById('item-carousel-2').innerHTML += card;
-    } else if (id > 6 && id <= 9) {
-      document.getElementById('item-carousel-3').innerHTML += card;
-    }
+    toys.innerHTML += item;
   } else if (prop === "healthcare_gatos") {
-    if (id <= 3) {
-      document.getElementById('item-carousel-4').innerHTML += card;
-    } else if (id > 3 && id <= 6) {
-      document.getElementById('item-carousel-5').innerHTML += card;
-    } else if (id > 6 && id <= 9) {
-      document.getElementById('item-carousel-6').innerHTML += card;
-    }
+    healthcare.innerHTML += item;
   }
 }
+
 function showProducts(products, property) {
   products[property].forEach(product => {
-    let card = `<div class="card border shadow" style="width: 18rem">
-    <div>
-    <img src="img/products/default.png" class="card-img-top img-fluid">
-    </div>
-    <div class="card-body d-flex justify-content-center align-items-center flex-column text-center">
-      <h2>${product.nombre}</h2>
-      <p class="card-text">${product.descripcion}</p>
-      <h3 class="card-text text-center"><strong>$${product.precio}</strong></h3>           
-    </div>
-    <div class="card-footer text-center">
-    <a href="#" class="btn btnBuy">Comprar</a>
-    </div>
-  </div>`;
+    let card = `
+                <div class="card border shadow" style="width: 18rem;">
+                  <div>
+                    <img src="img/products/default.png" class="card-img-top img-fluid">
+                  </div>
+                  <div class="card-body d-flex justify-content-center align-items-center flex-column text-center">
+                   <h2>${product.nombre}</h2>
+                    <p class="card-text">${product.descripcion}</p>
+                    <h3 class="card-text text-center"><strong>$${product.precio}</strong></h3>
+                    <p class="card-text">(${product.reviews} reviews)</p>          
+                  </div>
+                  <div class="card-footer text-center">
+                    <a href="#" class="btn btnBuy">Comprar</a>
+                  </div>
+                </div>
+    `;
+    let item = `
+                <div class="card" style="width: 18rem;">
+                <img src="img/products/default.png" class="card-img-top">
+                <div class="card-body">
+                  <h5>${product.nombre}</h5>
+                  <p>${product.descripcion}</p>
+                  <h3 class="card-text"><strong>$${product.precio}</strong></h3>
+                  <p><small>(${product.reviews} reviews)</small></p>
+                </div>
+              </div>                
+  `;
     fillCarousel(property, card, product.id);
+    fillCarouselSm(property, item);
   });
 }
 
